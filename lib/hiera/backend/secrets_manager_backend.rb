3588b38073381a160463d097b04c5be25ddd6ff9
adamarnold-msm <adam.arnold@moneysupermarket.com>
2021-02-08 15:42:11 +0000

    Add extra options which can be defined in hiera.yaml: * Optional IAM authentication * Optionally only lookup secrets matching a prefix * Optionally specify an envionment to use for lookups

--- lib/hiera/backend/secrets_manager_backend.rb
+++ lib/hiera/backend/secrets_manager_backend.rb
@@ -4,11 +4,30 @@ class Hiera
       def initialize
         require 'json'
         require 'aws-sdk-secretsmanager'
+        require 'hiera/interpolate'
         @config = Config
         @client = create_client
       end
 
       def lookup(key, scope, order_override, resolution_type)
+        if @config[:secrets_manager].key?(:confine_to_keys)
+          confine_keys = @config[:secrets_manager][:confine_to_keys]
+          unless confine_keys.is_a?(Array)
+            Hiera.warn("Hiera Secrets Manager confine_to_keys must be an array")
+          end
+
+          begin
+            confine_keys = confine_keys.map { |r| Regexp.new(r) }
+          rescue StandardError => err
+            Hiera.warn("Hiera Secrets Manager Failed to create regexp with error #{err}")
+          end
+          match = Regexp.union(confine_keys)
+          unless key[match] == key
+            Hiera.debug("Hiera Secrets Manager ignoring #{key}, not in confine_to_keys")
+            return nil
+          end
+        end
+
         answer = nil
 
         if @client.nil?
@@ -17,7 +36,7 @@ class Hiera
         end
 
         if contains_illegal_characters?(key)
-          Hiera.debug("#{key} contains illegal characters. Skipping lookup.")
+          Hiera.debug("Hiera Secrets Manager #{key} contains illegal characters. Skipping lookup.")
           return answer
         end
 
@@ -30,7 +49,9 @@ class Hiera
           when :hash
             answer = JSON.parse(retrieve_secret(key_to_query))
           else
+            key_to_query, key = key_to_query.split('::',2)
             answer = retrieve_secret(key_to_query)
+            answer = JSON.parse(answer)[key] if key
           end
         rescue Aws::SecretsManager::Errors::ResourceNotFoundException => error
           Hiera.debug("#{key_to_query} not found: #{error.message}")
@@ -47,11 +68,17 @@ class Hiera
       # AWS Secrets Manager only allows alphanumeric characters or (/_+=.@-) in key names
       # GetSecret requests will fail for keys which have illegal characters
       def contains_illegal_characters?(key)
-        %r{^[a-zA-Z0-9\/_+=.@\-]+$}.match(key).nil?
+        %r{^[a-zA-Z0-9\/_+=.@\-:]+$}.match(key).nil?
+      end
+
+      def parse_string(data, scope, extra_data={})
+        Hiera::Interpolate.interpolate(data, scope, extra_data)
       end
 
       def get_prefix(environments, scope)
-        if environments && environments.key?(scope['environment'])
+        if @config[:secrets_manager].has_key?(:env)
+          parse_string(@config[:secrets_manager][:env], scope)
+        elsif environments && environments.key?(scope['environment'])
           environments[scope['environment']]
         else
           scope['environment']
@@ -71,20 +98,27 @@ class Hiera
       def create_client
         if missing_config?
           Hiera.debug('Warning! Config is empty. Starting in a bad state.')
-          return nil
+
         end
 
-        if missing_keys?
+        if !@config[:secrets_manager][:iam_client] and missing_keys?
           Hiera.debug("Warning! Missing key(s) #{missing_keys} in Config. Starting in a bad state.")
           return nil
         end
 
         Hiera.debug('AWS Secrets Manager backend starting')
+        secrets_client
+      end
+
+      def secrets_client
+        if @config[:secrets_manager][:iam_client]
+          Hiera.debug('IAM Client')
+          return Aws::SecretsManager::Client.new(region: @config[:secrets_manager][:region])
+        end
         Aws::SecretsManager::Client.new(
-          region: @config[:secrets_manager][:region],
-          access_key_id: @config[:secrets_manager][:access_key_id],
-          secret_access_key: @config[:secrets_manager][:secret_access_key]
-        )
+            region: @config[:secrets_manager][:region],
+            access_key_id: @config[:secrets_manager][:access_key_id],
+            secret_access_key: @config[:secrets_manager][:secret_access_key])
       end
 
       def missing_config?
